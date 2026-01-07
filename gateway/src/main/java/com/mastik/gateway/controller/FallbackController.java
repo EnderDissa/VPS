cf6605c19e2ce8807ae9596b4947cfa2ff434ca2
RAZRULETEL <qwerty.qwertlain@gmail.com>
2026-01-07 21:59:43 +0300

    fix: fix gateway service config

--- gateway/src/main/java/com/mastik/gateway/controller/FallbackController.java
+++ gateway/src/main/java/com/mastik/gateway/controller/FallbackController.java
@@ -1,19 +1,21 @@
 package com.mastik.gateway.controller;
 
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 public class FallbackController {
 
-    @GetMapping("/fallback/service1")
-    public ResponseEntity<String> service1Fallback() {
-        return ResponseEntity.ok("Service 1 is currently unavailable. Please try again later.");
+    @GetMapping("/fallback/{service}")
+    public ResponseEntity<String> serviceFallback(@PathVariable String service) {
+        return new ResponseEntity<String>("{\"message\": \"" + service + " is currently unavailable. Please try again later.\"}", HttpStatusCode.valueOf(503));
     }
 
-    @GetMapping("/fallback/service2")
-    public ResponseEntity<String> service2Fallback() {
-        return ResponseEntity.ok("Service 2 is currently unavailable. Please try again later.");
-    }
 }
